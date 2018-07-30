b4b805cfe14164e28e86699a3c4363a22db49c4b
zhouhuamin <523141399@qq.com>
2018-07-30 20:39:39 +0800

    add exception handling disabled, use -fexceptions to enable

--- main.cpp
+++ main.cpp
@@ -3,6 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+/*
+现象
+在使用C++开发Android过程中采用了C++的Exception机制，因而导致NDK编译失败，抛出错误: exception handling disabled, use -fexceptions to enable
+
+原因
+此问题的出现是编译器的异常异常捕获被禁用了，需要在Android.mk文件中开启。
+
+解决方案
+在Android.mk文件中添加： LOCAL_CPPFLAGS += -fexceptions就可以了。
+在Application.mk文件中添加APP_CPPFLAGS += -fexceptions，这种配置会自动应用到工程的所有模块当中 
+
+*/
+
+
 #include <cstdlib>
 #include <iostream>
 #include <thread>
